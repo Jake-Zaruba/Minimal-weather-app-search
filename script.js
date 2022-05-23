6fd347daa3098c8172e0f7f65d22ca7b31e9783a
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2022-05-22 19:06:08 -0500

    Added install button

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ if (navigator.serviceWorker) {
   });
 }
 
+let downloadBtn = document.querySelector(`.download`);
+
+let deferredPrompt;
+window.addEventListener(`beforeinstallprompt`, (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  downloadBtn.style.display = `block`;
+});
+
+downloadBtn.addEventListener(`click`, (e) => {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === `accepted`) {
+      console.log(`User accepted the A2HS prompt`);
+    }
+    deferredPrompt = null;
+  });
+});
+
 //////////////////////////////
 /////FORECAST BY LOCATION/////
 //////////////////////////////
